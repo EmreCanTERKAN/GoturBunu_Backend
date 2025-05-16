1a7aae0ab42b65b839b187dbe539c6e40139302c
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-05-16 19:45:21 +0300

    Add District Province and carrierRegistryRequest

---
+++ GoturBunu_Backend.Domain/Entities/Carrier/CarrierRegistryRequest.cs
@@ -0,0 +1,15 @@
+﻿using GoturBunu_Backend.Domain.Abstractions;
+using GoturBunu_Backend.Domain.Enums;
+
+namespace GoturBunu_Backend.Domain.Entities.Carrier;
+public sealed class CarrierRegistryRequest : Entity
+{
+    public string Name { get; set; } = default!;
+    public string SurName { get; set; } = default!;
+    public string NationalIdentityNumber { get; set; } = default!;
+    public string Phone { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public ERegisteryRequestStatus Status { get; set; } = ERegisteryRequestStatus.Pending;
+    public string DistrictId { get; set; } = default!;
+    public District District { get; set; } = default!;
+}
---
+++ GoturBunu_Backend.Domain/Entities/District.cs
@@ -0,0 +1,8 @@
+﻿using GoturBunu_Backend.Domain.Abstractions;
+
+namespace GoturBunu_Backend.Domain.Entities;
+public sealed class District : Entity
+{
+    public string Name { get; set; } = default!;
+    public Province Province { get; set; } = default!;
+}
